d05a97ad94145556dcf0b5e8a1f0ec1834293261
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-06-22 12:27:48 +0200

    add logic to middleware functions

---
+++ Middleware/middleware.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+function authorization(req, res, next) {
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    return res.status(403).json({ error: 'You have no access here' });
+  }
+
+  try {
+    const data = jwt.verify(token, process.env.SECRET);
+    req.userId = data.id;
+    req.userRole = data.role;
+    return next();
+  } catch {
+    return res.status(403).json({ error: 'You have an invalid token' });
+  }
+}
+
+module.exports = { authorization };

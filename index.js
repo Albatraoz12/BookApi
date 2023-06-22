a9b0f5d5ba6961a2fd76b9689a7a45407833ee1a
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-06-22 12:27:54 +0200

    add comments

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ require('dotenv').config();
 // initialize server
 const app = express();
 
+// Setup cors
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://jwtfrontend.onrender.com'],
+  })
+);
+app.use(express.json()); // Enables to send json as response
+app.use(cookieParser()); // Enables to set cookies to client
+
 // Add connection to mongodb
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-app.use(express.json());
-app.use(cookieParser());
 
 // Start the server on port 8080
 const port = 8080;

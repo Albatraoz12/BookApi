04fefb3e06fb95c04aa580c2d407ebcf3e810a27
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-06-22 14:49:29 +0200

    create book model

---
+++ Model/Book.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  author: String,
+  image: {
+    data: Buffer,
+    contentType: String,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+module.exports = mongoose.model('Book', BookSchema);

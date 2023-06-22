83a1cbb3b29b4180c41a72d499dbcaf490e91166
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-06-22 14:24:55 +0200

    update usercontroller with tryCatch

--- Controller/userController.js
+++ Controller/userController.js
@@ -2,28 +2,28 @@ const User = require('../Model/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-const userRegistration = (req, res) => {
+const userRegistration = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    // console.log(`username: ${username} password: ${password}, email: ${email}`);
     if (!username || !email || !password)
-      return res.status(401).json({ error: 'please fill in the feilds' });
+      return res.status(401).json({ error: 'please fill in the fields' });
 
-    User.findOne({ email: email }, (err, user) => {
-      if (err) console.log(err);
-      if (user) {
-        return res.status(400).json({ error: 'user already exist' });
-      }
-      const newUser = new User({
-        username: username,
-        email: email,
-        password: password,
-      });
-      newUser.password = bcrypt.hashSync(newUser.password, 10);
-      newUser.save().then(() => {
-        res.status(201).json({
-          message: 'New user has been created!',
-        });
-      });
+    const existingUser = await User.findOne({ email: email }).exec();
+    if (existingUser) {
+      return res.status(400).json({ error: 'user already exists' });
+    }
+
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: password,
+    });
+    newUser.password = bcrypt.hashSync(newUser.password, 10);
+    await newUser.save();
+
+    res.status(201).json({
+      message: 'New user has been created!',
     });
   } catch (error) {
     res.status(500).json({ message: error });
@@ -31,38 +31,42 @@ const userRegistration = (req, res) => {
 };
 
 const userLogin = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
+    console.log(`password: ${password}, email: ${email}`);
+    if (req.cookies.access_token)
+      return res.status(401).json({ message: 'You are already logged in' });
 
-  if (req.cookies.access_token)
-    return res.status(401).json({ message: 'You are already logged in' });
+    const user = await User.findOne({ email });
 
-  const user = await User.findOne({ email });
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
 
-  if (user) {
-    const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
+        const token = jwt.sign(
+          {
+            id: user._id,
+            role: user.role,
+          },
+          process.env.SECRET
+        );
 
-    if (passwordMatch) {
-      const token = jwt.sign(
-        {
-          id: user._id,
-          role: user.role,
-        },
-        process.env.SECRET
-      );
-
-      return res
-        .cookie('access_token', token, {
-          httpOnly: true,
-          sameSite: 'none',
-          secure: true,
-        })
-        .status(200)
-        .json({ message: 'Logged in successfully 😊 👌' });
-    } else if (!passwordMatch) {
-      res.status(400).json({ message: 'Wrong Password, try again' });
+        return res
+          .cookie('access_token', token, {
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true,
+          })
+          .status(200)
+          .json({ message: 'Logged in successfully 😊 👌' });
+      } else if (!passwordMatch) {
+        res.status(400).json({ message: 'Wrong Password, try again' });
+      }
+    } else {
+      res.status(400).json({ message: 'sorry, could not login' });
     }
-  } else {
-    res.status(400).json({ message: 'sorry, could not login' });
+  } catch (error) {
+    console.log(error);
   }
 };
 

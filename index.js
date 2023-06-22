71ee0c1c0369f8d65ce3e840c94c494ffb490176
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-06-22 14:24:42 +0200

    add logs to see if server is connected to MB

--- index.js
+++ index.js
@@ -22,10 +22,27 @@ app.use(cookieParser()); // Enables to set cookies to client
 //Routes
 app.use('/api', userRouter);
 
-// Add connection to mongodb
+app.get('/', (req, res) => {
+  const test = req.body;
+  console.log(req.body);
+  return res.status(200).json({ message: 'It Works!!!', yourMessage: test });
+});
+
+// Add connection to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+// Capture MongoDB connection event
+const db = mongoose.connection;
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Capture MongoDB connection error event
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
 });
 
 // Start the server on port 8080

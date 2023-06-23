f0dc1c3e4a7cdd9a898e673c8bde811fce3bcefc
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-06-23 11:59:05 +0200

    create logic for creating a book

--- Controller/bookController.js
+++ Controller/bookController.js
@@ -4,10 +4,14 @@ const Book = require('../Model/Book');
 const createBook = async (req, res) => {
   try {
     const validUser = await User.findOne({ _id: req.userId });
+
     if (!validUser)
-      return res.status(401).json({ message: 'You are not authorized' });
+      return res
+        .status(404)
+        .json({ message: 'You are not authorized or logged in' });
 
     const { title, description, imagePath, imageName } = req.body;
+    console.log(`${title},${description},${imagePath},${imageName}`);
     const newBook = new Book({
       title: title,
       description: description,
@@ -18,7 +22,7 @@ const createBook = async (req, res) => {
     newBook.save();
     res.status(200).json({
       message: 'Book has been created! 😏 🍀',
-      post: newPost,
+      Book: newBook,
     });
   } catch (error) {
     console.error(error.message);

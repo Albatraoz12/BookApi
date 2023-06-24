deb4d0e953cb7d95f0000276a53733f1ff1a2b23
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-06-24 21:40:56 +0200

    Updated multer disk storage configuration to retain original filenames with a unique suffix

--- Routes/BookRoute.js
+++ Routes/BookRoute.js
@@ -18,14 +18,20 @@ if (!fs.existsSync(uploadDirectory)) {
   fs.mkdirSync(uploadDirectory);
 }
 
-// Configure multer storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads'); // Specify the destination folder for storing the uploaded files
+    cb(null, 'uploads');
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9); // Add a unique suffix to the filename
-    cb(null, file.fieldname + '-' + uniqueSuffix);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const originalNameWithoutExtension = file.originalname.split('.')[0];
+    cb(
+      null,
+      originalNameWithoutExtension +
+        '-' +
+        uniqueSuffix +
+        path.extname(file.originalname)
+    );
   },
 });
 

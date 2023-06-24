82a1799765f667c552a6f3fbbed0f32549fb53b0
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-06-24 16:43:25 +0200

    robust the code better

--- Controller/bookController.js
+++ Controller/bookController.js
@@ -6,9 +6,10 @@ const mongoose = require('mongoose');
 const getBooks = async (req, res) => {
   try {
     const getAllBooks = await Book.find();
-    res.status(200).json({ Books: getAllBooks });
+    res.status(200).json({ data: getAllBooks });
   } catch (error) {
-    res.send(500).json({ error: error.message });
+    console.error(error);
+    res.status(500).json({ error: 'Error while retrieving books' });
   }
 };
 
@@ -18,7 +19,7 @@ const getBook = async (req, res) => {
     const bookId = req.params.id;
 
     if (!mongoose.Types.ObjectId.isValid(bookId)) {
-      return res.status(400).json({ message: 'Invalid book ID' });
+      return res.status(400).json({ error: 'Invalid book ID' });
     }
 
     const getBookId = await Book.findById(bookId).populate({
@@ -27,26 +28,22 @@ const getBook = async (req, res) => {
     });
 
     if (!getBookId) {
-      return res.status(404).json({ message: 'There is no book with that ID' });
+      return res.status(404).json({ error: 'Book not found' });
     }
 
-    // Check if the book has an image
     if (
       getBookId.image &&
       getBookId.image.data &&
       getBookId.image.contentType
     ) {
-      // Set the response content type based on the image's contentType
       res.contentType(getBookId.image.contentType);
-
-      // Send the image data
       res.status(200).json(getBookId.image.data);
     } else {
-      // If the book doesn't have an image, send the book details without the image data
-      res.status(200).json({ book: getBookId });
+      res.status(200).json({ data: getBookId });
     }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error(error);
+    res.status(500).json({ error: 'Error while retrieving book' });
   }
 };
 
@@ -55,15 +52,16 @@ const createBook = async (req, res) => {
   try {
     const validUser = await User.findOne({ _id: req.userId });
 
-    if (!validUser)
+    if (!validUser) {
       return res
         .status(404)
-        .json({ message: 'You are not authorized or logged in' });
+        .json({ error: 'You are not authorized or logged in' });
+    }
 
     const { title, description } = req.body;
     const imagePath = req.file.path;
     const imageName = req.file.filename;
-    console.log(`${title},${description},${imagePath},${imageName}`);
+
     const newBook = new Book({
       title: title,
       description: description,
@@ -71,14 +69,16 @@ const createBook = async (req, res) => {
       imageName: imageName,
       owner: req.userId,
     });
-    newBook.save();
+
+    await newBook.save();
+
     res.status(200).json({
-      message: 'Book has been created! 😏 🍀',
-      Book: newBook,
+      message: 'Book has been created!',
+      data: newBook,
     });
   } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: 'error vile creating your book!' });
+    console.error(error);
+    res.status(500).json({ error: 'Error while creating your book' });
   }
 };
 
@@ -88,25 +88,31 @@ const deleteBook = async (req, res) => {
     const bookId = req.params.id;
     const userId = req.userId;
 
-    if (!bookId)
+    if (!bookId) {
       return res.status(400).json({ error: 'Please provide a valid id' });
+    }
 
     const book = await Book.findById(bookId).populate({
       path: 'owner',
       select: 'id',
     });
 
-    if (book.owner.id !== userId)
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+
+    if (book.owner.id !== userId) {
       return res
-        .status(200)
+        .status(403)
         .json({ error: 'You are not the owner of this book' });
+    }
 
-    book.deleteOne();
+    await book.deleteOne();
 
-    res.status(200).json({ message: 'You book has bow been deleted' });
+    res.status(200).json({ message: 'Your book has been deleted' });
   } catch (error) {
-    res.status(500).json({ error: error.message });
-    console.log(error);
+    console.error(error);
+    res.status(500).json({ error: 'Error while deleting book' });
   }
 };
 
